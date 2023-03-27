c862975d1847bccf702c4ff1b3645be723462006
Markus Loeffler <m_loeffler@gmx.de>
2023-03-27 16:58:29 +0100

    implement recipe display

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('Lasagne', 'Delicious Spinach Lasagne', 'https://minimalistbaker.com/wp-content/uploads/2021/08/Butternut-Squash-Lasagna-Vegan-GF-Optional-Fall-inspired-veggie-packed-incredibly-comforting-and-just-10-ingredients-required-minimalistbaker-recipe-plantbased-glutenfree-lasagna-squash-10.jpg')
+  ]
   ngOnInit(): void {
   }
 
